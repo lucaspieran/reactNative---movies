b82606db76a690bee50b4137e2ddcc78b673bcd3
Lucas <lucaspierandrei19@gmail.com>
2021-05-07 12:18:51 -0300

    add files

---
+++ src/screens/FadeScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const FadeScreen = () => {
+  return (
+  <View style={{
+    flex: 1, 
+    backgroundColor: "red",
+    justifyContent :"center",
+    alignItems:"center"
+    }}>
+
+  </View>
+  );
+};
+
+export default FadeScreen;
